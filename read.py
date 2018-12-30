1f972ff05321fc435091b5f7dff34f69c94aa117
sampr <samprhub@gmail.com>
2018-12-30 16:51:21 -0500

    Initial function structure

---
+++ read.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+def readIn():
+
+	family = dict()
+
+	#Continuously take input for each person and add to map
+
+	return family
+
+
+def toJson():
+
+	#Write map to json and export to file
+
+	return 0
+
+def main():
+
+	familyData = readIn()
+
+	return 0
+
+if __name__ == '__main__':
+    main()
